6e739fd9e049bd4eb174d31e33e9d57bb2d93b9a
mozso <126395568+m0zs0@users.noreply.github.com>
2024-03-25 11:19:22 +0100

    Add project files.

---
+++ array_coworkers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array_coworkers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Név1: Tömb létrehozása
+
+            //Név2: Tömb adatainak összeadása
+
+            //Név3: Eredmények kiírása
+
+
+        }
+    }
+}
